a263da1640dd6cd83422a08fd62ba4d998210ab2
SertoriusX <152284852+SertoriusX@users.noreply.github.com>
2025-06-27 14:22:57 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
 
+  let whoItem = who[Math.floor(Math.random() * who.length)];
+  let actionItem = action[Math.floor(Math.random() * action.length)];
+  let whatItem = what[Math.floor(Math.random() * what.length)];
+  let whenItem = when[Math.floor(Math.random() * when.length)];
+  return `
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+<div class="text-body">
+<h1 >OMG! you will not believe me but ...</h1>
+${whoItem} ${actionItem} ${whatItem} ${whenItem}
+</div>`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
+  
